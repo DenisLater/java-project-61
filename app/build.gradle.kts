909fc490a6d1a787df54088a89ffea339661f11c
DenisLater <cool.aweson@yandex.ru>
2025-06-19 21:24:36 +0300

    Добавлены исходные файлы проекта

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("java")
+    application
+}
+
+application {
+    mainClass.set("hexlet.code.App")  // Убрал слеш в начале
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+
